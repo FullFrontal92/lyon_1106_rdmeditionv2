04d94130c4ddc43088ec65cc40106a58767bae23
AymenMech <aymen.mechiche@gmail.com>
2016-12-07 17:43:57 +0100

    added function on cart.js for img cart button menu image

--- web/js/cart.js
+++ web/js/cart.js
@@ -3,8 +3,15 @@ $(document).ready(function() {
         if ( $(".popup").hasClass("top")){
             $(".popup").removeClass("top");
             $(".popup").addClass("bottom");
-            $(".popup").css("top","30px");
+            $(".popup").css("top","48px");
             $(".popup").css("bottom","auto");
         }
+    });
+
+    $('#sylius-cart-button').mouseenter(function() {
+        $(".basketIcon").attr("src","/img/panierviolet.png" )
+    });
+    $('#sylius-cart-button').mouseleave(function() {
+        $(".basketIcon").attr("src","/img/panier.png" )
     });
 });
