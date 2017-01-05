28bccab61026bf41b58d107da738d389011908e5
AymenMech <aymen.mechiche@gmail.com>
2017-01-05 22:50:24 +0100

    bug correction on stamp.js and customisation of form product

--- web/js/stamp.js
+++ web/js/stamp.js
@@ -3,59 +3,62 @@ $(document).ready(function() {
     var news = $('#sylius_product_new');
     var preorder =$('#sylius_product_preOrder');
 
-    // Block Stamp checkbox so only one can be enable for a product
-
-    if ( $(promo).attr('checked', 'checked') ){
-        $(promo).addClass('selected');
-        $(preorder).prop('disabled', true);
-        $(news).prop('disabled', true);
-    } else if ( $(news).attr('checked', 'checked') ){
-        $(news).addClass('selected');
-        $(preorder).prop('disabled', true);
-        $(promo).prop('disabled', true);
-    }else if( $(preorder).attr('checked', 'checked') ){
-        $(preorder).addClass('selected')
-        $(promo).prop('disabled', true);
-        $(news).prop('disabled', true);
-    }
-
     function disable($a, $b) {
-            $($a).prop('disabled', true);
-            $($b).prop('disabled', true);
+        $($a).prop('disabled', true);
+        $($b).prop('disabled', true);
     }
     function enable($a, $b) {
-        $($a).removeAttr('disabled');
-        $($b).removeAttr('disabled');
+        $a.removeAttr('disabled');
+        $b.removeAttr('disabled');
+    }
+    var promoCheck = promo.attr("checked");
+    var newsCheck = news.attr("checked");
+    var preOrderCheck = preorder.attr("checked");
+    console.log(promoCheck);
+    // Block Stamp checkbox so only one can be enable for a product
+
+    if ( promoCheck == 'checked') {
+        promo.addClass('selected');
+        preorder.prop('disabled', true);
+        news.prop('disabled', true);
+    } else if (newsCheck == 'checked' == 'checked') {
+        news.addClass('selected');
+        preorder.prop('disabled', true);
+        promo.prop('disabled', true);
+    } else if ( preOrderCheck == 'checked' == 'checked') {
+        preorder.addClass('selected');
+        promo.prop('disabled', true);
+        news.prop('disabled', true);
     }
 
 
     $("label[for='sylius_product_promo']").click(function () {
-        if ($(promo).hasClass("selected")){
+        if (promo.hasClass("selected")){
             enable(news, preorder);
-            $(promo).removeClass("selected");
-        } else {
+            promo.removeClass("selected");
+        } else if (!news.hasClass("selected") && !preorder.hasClass("selected")) {
             disable(news, preorder);
-            $(promo).addClass("selected");
+            promo.addClass("selected");
         }
     });
     $("label[for='sylius_product_new']").click(function () {
 
-        if ($(news).hasClass("selected")){
+        if (news.hasClass("selected")){
             enable(promo, preorder);
-            $(news).removeClass("selected");
-        } else {
+            news.removeClass("selected");
+        } else if (!promo.hasClass("selected") && !preorder.hasClass("selected")){
             disable(promo, preorder);
-            $(news).addClass("selected");
+            news.addClass("selected");
         }
     });
     $("label[for='sylius_product_preOrder']").click(function () {
 
-        if ($(preorder).hasClass("selected")){
+        if (preorder.hasClass("selected")){
             enable(news, promo);
-            $(preorder).removeClass("selected");
-        } else {
+            preorder.removeClass("selected");
+        } else if (!promo.hasClass("selected") && !news.hasClass("selected")) {
             disable(news, promo);
-            $(preorder).addClass("selected");
+            preorder.addClass("selected");
         }
     });
 });
