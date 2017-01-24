8506c6fff41941fecd4510495b1d1a13c42dd803
AymenMech <aymen.mechiche@gmail.com>
2017-01-24 16:00:08 +0100

    working on eventListener, extended Order model

--- src/AppBundle/Entity/Order.php
+++ src/AppBundle/Entity/Order.php
@@ -3,7 +3,25 @@
 namespace AppBundle\Entity;
 
 use Sylius\Component\Core\Model\Order as BaseOrder;
+
 class Order extends BaseOrder
 {
-
+    public function sendEmailConfirmationOrder()
+    {
+        $generatedPdfFilename = $this->get('kernel')->getRootDir() . '/Resources/SyliusShopBundle/views/Facture5.pdf';
+        if(isset($generatedPdfFilename)){
+            unlink($generatedPdfFilename);
+        }
+        $this->get('knp_snappy.pdf')->generateFromHtml(
+            $this->renderView(
+                'SyliusCoreBundle:Email:pdf.html.twig',
+                array(
+                    'product'  => $this->getItems(),
+                    'client'  => $this->getUser(),
+                    'total' => $this->getPayments()
+                )
+            ),
+            $generatedPdfFilename
+                     );
+    }
 }
